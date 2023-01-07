3fd81884317fdf7a811e2d35371036ab331d9409
Chen Zhou <zhouchenclyde@gmail.com>
2023-01-07 15:51:55 -0700

    login-UI init

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { LoginForm } from './components/loginForm'
+import React, { Component } from 'react';
+
+function App() {
+  return (
+    
+      <div>
+        <LoginForm />
+        </div>
+    
+  );
+}
+
+export default App;
