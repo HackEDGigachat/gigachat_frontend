30e87ece4509cc8cf799d036edb3130b24325c83
zxlim92 <55634410+zxlim92@users.noreply.github.com>
2023-01-07 20:23:47 -0700

    navigation

---
+++ frontend/src/pages/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Test(){
+    return (
+        <div>
+            Test
+        </div>
+    )
+}
+
+export default Test
